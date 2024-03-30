aa2fe143b8b88e9a4aa9bbe9def4e0d4f307fe77
jbrig <jerusa.brignardello@gmail.com>
2024-03-30 19:54:41 +0000

    enhance clarity and readibility

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,16 @@
+"""Implement Changes:
+Add comments to each part of the code for better readability.
+Replace the .format method with an f-string for clarity and improved performance.
+"""
 
-# First I defined the first variable to ask as user_name and using input to request this information
-#Then I defined the second variables to ask as user age and using input to request this information
-# In order to print the requested information, I wrote a phrase between brackets to give it a grammatical sense to the requested information
-#In this phrase, I used the F string format where I included both previous defined variables
-# Finally, I printed the last phrase.
-user_name = input("What is your name ?") 
-user_age = input("Tell me your age, please")
-print("Your name is {} and you are {} old".format (user_name, user_age))
-print("Hello World !")
+# Ask the user for their name
+user_name = input("What is your name? ")
+
+# Ask the user for their age
+user_age = input("Tell me your age, please: ")
+
+# Display the collected information
+print(f"Your name is {user_name} and you are {user_age} years old.")
+
+# Greet the world
+print("Hello World!")
